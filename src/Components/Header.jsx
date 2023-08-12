a690ed0fdc612891141e9398bfcc4b809fa3216a
Vishnu <vishnu@enovasolutions.com>
2023-08-12 16:11:12 +0530

    Tailwind initialized

---
+++ src/Components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className=''>
+        <h1 className='font-bold italic text-center pt-5 text-5xl font '>STAR</h1>
+    </div>
+  )
+}
+
+export default Header
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {},
+    colors:
+    {
+      'white':'#ffffff',
+      'primary':'0166FF',
+    }
+  },
+  plugins: [],
+}
+
